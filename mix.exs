f75b8c5062d20c7ccc5351054916cc7b81c77609
av <av@fun-box.ru>
2017-06-11 22:45:21 +0300

    Allow Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Exec.Mixfile do
       app: :exec,
       compilers: [:exec] ++ Mix.compilers,
       version: "0.1.0",
-      elixir: "~> 1.4",
+      elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps()
